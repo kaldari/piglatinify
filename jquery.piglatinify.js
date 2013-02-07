b94f5e49d1b33a50208af61a4239ab22f0b8992b
Kaldari <rkaldari@wikimedia.org>
2013-02-07 15:50:29 -0800

    better function name

--- jquery.piglatinify.js
+++ jquery.piglatinify.js
@@ -2,7 +2,7 @@
 
 	$.fn.piglatinify = function() {
 	
-		piglatin = function( node ) {
+		convertWord = function( node ) {
 			var textArray = node.split(" ");
 			$( textArray ).each( function( index ) {
 				var firstLetter = textArray[index].charAt(0);
@@ -17,7 +17,7 @@
 				tw = document.createTreeWalker( el, NodeFilter.SHOW_TEXT );
 			while ( tw.nextNode() ) {
 				node = tw.currentNode;
-				node.nodeValue = piglatin( node.nodeValue );
+				node.nodeValue = convertWord( node.nodeValue );
 			}
 		} );
 	};
