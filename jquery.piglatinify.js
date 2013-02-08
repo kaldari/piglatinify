603bc78c118b3985c1a13b02cdfc5cc01ee48884
Kaldari <rkaldari@wikimedia.org>
2013-02-07 16:44:03 -0800

    fix edge cases

--- jquery.piglatinify.js
+++ jquery.piglatinify.js
@@ -1,16 +1,20 @@
 ( function ( $ ) {
 
 	$.fn.piglatinify = function() {
+		var punctuation = ['.', '!', '?', ';', ':', '\'', '"', ','];
 
 		convertWord = function( node ) {
 			var textArray = node.split(" ");
 			$( textArray ).each( function( index ) {
-				var firstLetter = textArray[index].charAt(0);
-				textArray[index] = textArray[index].substring(1) + firstLetter + 'ay';
+				// Make sure word isn't empty or just punctuation
+				if ( textArray[index].length > 0 && $.inArray( textArray[index], punctuation ) === -1 ) {
+					var firstLetter = textArray[index].charAt(0);
+					textArray[index] = textArray[index].substring(1) + firstLetter + 'ay';
+				}
 			} );
 			return textArray.join(" ");
 		};
-	
+
 		return this.each( function( index, element ) {
 			var node,
 				tw = document.createTreeWalker( element, NodeFilter.SHOW_TEXT );
